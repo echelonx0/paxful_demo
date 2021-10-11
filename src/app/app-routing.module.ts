0b6c1102ff7d0fd716ff3dd075fffc8699478424
Uchenna Igwe <ig.ultrabix@gmail.com>
2021-10-11 06:39:10 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssistantComponent } from './components/assistant/assistant.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { InitiateComponent } from './components/initiate/initiate.component';
+import { ResultsComponent } from './components/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'search', component: AssistantComponent},
+  {path: 'hero', component: HeroComponent},
+  {path: 'results', component: ResultsComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'initiate', component: InitiateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AssistantComponent } from './components/assistant/assistant.component';
+import { HomeComponent } from './components/home/home.component';
+import { ResultsComponent } from './components/results/results.component';
+import { InitiateComponent } from './components/initiate/initiate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent,
+    DashboardComponent,
+    AssistantComponent,
+    HomeComponent,
+    ResultsComponent,
+    InitiateComponent
   ],
   imports: [
     BrowserModule,
